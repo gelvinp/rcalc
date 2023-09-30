364d7444765a0b74003ab0effab7a183c14c62b7
Patrick Gelvin <patrick@gelvin.dev>
2023-09-30 09:41:25 -0700

    Vendored glm

---
+++ modules/glm/register_module.cpp
@@ -0,0 +1,4 @@
+#include "register_module.h"
+
+void initialize_glm_module() {}
+void cleanup_glm_module() {}
---
+++ modules/glm/register_module.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void initialize_glm_module();
+void cleanup_glm_module();
