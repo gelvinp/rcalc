f866f9679292dcc8f32b3503614adce08a089a85
Patrick Gelvin <patrick@gelvin.dev>
2023-10-03 20:05:46 -0700

    Unify main

--- main/main.cpp
+++ main/main.cpp
@@ -1,9 +1,36 @@
 #include "core/logger.h"
 #include "core/version.h"
 #include "main/main.h"
+#include "platform/platform.h"
+#include "modules/register_modules.h"
 
 #include <iostream>
 #include <string_view>
+#include <sstream>
+
+
+int main(int argc, char** pp_argv)
+{
+    Main m;
+    Platform& platform = Platform::get_singleton();
+
+    m.parse_args(argc, pp_argv);
+    initialize_modules();
+    Result<> res = platform.init();
+    
+    if (!res) {
+        std::stringstream ss;
+        ss << res.unwrap_err();
+        Logger::log_err("%s", ss.str().c_str());
+    } else {
+        platform.runloop();
+    }
+
+    platform.cleanup();
+    cleanup_modules();
+
+    return res ? 0 : 255;
+}
 
 
 void Main::parse_args(int argc, char** pp_argv)
