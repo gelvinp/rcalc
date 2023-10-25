be52066f1b3f3e29e5075f96f21aef55597a769e
Patrick Gelvin <patrick@gelvin.dev>
2023-10-24 23:09:32 -0700

    Separate out matrix formatting for use in operators

--- core/format.h
+++ core/format.h
@@ -1,9 +1,15 @@
 #pragma once
 
+#include "core/value.h"
+
 #include <string>
 
 namespace RCalc {
 
 std::string fmt(const char* p_format, ...);
+
+std::string fmt_mat2(Mat2 value);
+std::string fmt_mat3(Mat3 value);
+std::string fmt_mat4(Mat4 value);
 
 }
