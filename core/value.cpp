5ffa08c21dce63d289c20f9f4f4af6536ae9c45d
Patrick Gelvin <patrick@gelvin.dev>
2023-10-24 22:55:05 -0700

    Properly display matrices in ImGuiRenderer stack (Breaks help page)

--- core/value.cpp
+++ core/value.cpp
@@ -654,15 +654,125 @@ std::string Value::to_string() const {
         }
         case TYPE_MAT2: {
             const Mat2 value = operator Mat2();
-            return fmt("{[%g, %g], [%g, %g]}", value[0].x, value[1].x, value[0].y, value[1].y);
+
+            int col0_len = std::max(
+                snprintf(nullptr, 0, "%g", value[0].x),
+                snprintf(nullptr, 0, "%g", value[0].y)
+            );
+            int col1_len = std::max(
+                snprintf(nullptr, 0, "%g", value[1].x),
+                snprintf(nullptr, 0, "%g", value[1].y)
+            );
+
+            return fmt(
+                "| %*g, %*g |\n| %*g, %*g |",
+                col0_len, value[0].x, 
+                col1_len, value[1].x,
+                col0_len, value[0].y,
+                col1_len, value[1].y
+            );
         }
         case TYPE_MAT3: {
             const Mat3 value = operator Mat3();
-            return fmt("{[%g, %g, %g], [%g, %g, %g], [%g, %g, %g]}", value[0].x, value[1].x, value[2].x, value[0].y, value[1].y, value[2].y, value[0].z, value[1].z, value[2].z);
+
+            int col0_len = std::max(
+                snprintf(nullptr, 0, "%g", value[0].x),
+                std::max(
+                    snprintf(nullptr, 0, "%g", value[0].y),
+                    snprintf(nullptr, 0, "%g", value[0].z)
+                )
+            );
+            int col1_len = std::max(
+                snprintf(nullptr, 0, "%g", value[1].x),
+                std::max(
+                    snprintf(nullptr, 0, "%g", value[1].y),
+                    snprintf(nullptr, 0, "%g", value[1].z)
+                )
+            );
+            int col2_len = std::max(
+                snprintf(nullptr, 0, "%g", value[2].x),
+                std::max(
+                    snprintf(nullptr, 0, "%g", value[2].y),
+                    snprintf(nullptr, 0, "%g", value[2].z)
+                )
+            );
+
+            return fmt(
+                "| %*g, %*g, %*g |\n| %*g, %*g, %*g |\n| %*g, %*g, %*g |",
+                col0_len, value[0].x,
+                col1_len, value[1].x,
+                col2_len, value[2].x,
+                col0_len, value[0].y,
+                col1_len, value[1].y,
+                col2_len, value[2].y,
+                col0_len, value[0].z,
+                col1_len, value[1].z,
+                col2_len, value[2].z
+            );
         }
         case TYPE_MAT4: {
             const Mat4 value = operator Mat4();
-            return fmt("{[%g, %g, %g, %g], [%g, %g, %g, %g], [%g, %g, %g, %g], [%g, %g, %g, %g]}", value[0].x, value[1].x, value[2].x, value[3].x, value[0].y, value[1].y, value[2].y, value[3].y, value[0].z, value[1].z, value[2].z, value[3].z, value[0].w, value[1].w, value[2].w, value[3].w);
+
+            int col0_len = std::max(
+                snprintf(nullptr, 0, "%g", value[0].x),
+                std::max(
+                    snprintf(nullptr, 0, "%g", value[0].y),
+                    std::max(
+                        snprintf(nullptr, 0, "%g", value[0].z),
+                        snprintf(nullptr, 0, "%g", value[0].w)
+                    )
+                )
+            );
+            int col1_len = std::max(
+                snprintf(nullptr, 0, "%g", value[1].x),
+                std::max(
+                    snprintf(nullptr, 0, "%g", value[1].y),
+                    std::max(
+                        snprintf(nullptr, 0, "%g", value[1].z),
+                        snprintf(nullptr, 0, "%g", value[1].w)
+                    )
+                )
+            );
+            int col2_len = std::max(
+                snprintf(nullptr, 0, "%g", value[2].x),
+                std::max(
+                    snprintf(nullptr, 0, "%g", value[2].y),
+                    std::max(
+                        snprintf(nullptr, 0, "%g", value[2].z),
+                        snprintf(nullptr, 0, "%g", value[2].w)
+                    )
+                )
+            );
+            int col3_len = std::max(
+                snprintf(nullptr, 0, "%g", value[3].x),
+                std::max(
+                    snprintf(nullptr, 0, "%g", value[3].y),
+                    std::max(
+                        snprintf(nullptr, 0, "%g", value[3].z),
+                        snprintf(nullptr, 0, "%g", value[3].w)
+                    )
+                )
+            );
+
+            return fmt(
+                "| %*g, %*g, %*g, %*g |\n| %*g, %*g, %*g, %*g |\n| %*g, %*g, %*g, %*g |\n| %*g, %*g, %*g, %*g |",
+                col0_len, value[0].x,
+                col1_len, value[1].x,
+                col2_len, value[2].x,
+                col3_len, value[3].x,
+                col0_len, value[0].y,
+                col1_len, value[1].y,
+                col2_len, value[2].y,
+                col3_len, value[3].y,
+                col0_len, value[0].z,
+                col1_len, value[1].z,
+                col2_len, value[2].z,
+                col3_len, value[3].z,
+                col0_len, value[0].w,
+                col1_len, value[1].w,
+                col2_len, value[2].w,
+                col3_len, value[3].w
+            );
         }
         case TYPE_UNIT: {
             const Unit value = operator Unit();
