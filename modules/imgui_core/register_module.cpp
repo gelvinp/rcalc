3292075b41bb7402ec9bba80db01272eb57a5162
Patrick Gelvin <patrick@gelvin.dev>
2023-12-01 11:51:58 -0700

    Set ImGui to use new allocator

---
+++ modules/imgui_core/register_module.cpp
@@ -0,0 +1,17 @@
+#include "register_module.h"
+
+#include "imgui.h"
+#include "core/memory/allocator.h"
+
+void initialize_imgui_core_module() {
+    ImGuiMemAllocFunc imgui_alloc = [](size_t size_bytes, void* p_udata) {
+        return Allocator::alloc(size_bytes);
+    };
+    ImGuiMemFreeFunc imgui_free = [](void* ptr, void* p_udata) {
+        Allocator::free(ptr);
+    };
+    
+    ImGui::SetAllocatorFunctions(imgui_alloc, imgui_free);
+};
+
+void cleanup_imgui_core_module() {}
---
+++ modules/imgui_core/register_module.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void initialize_imgui_core_module();
+void cleanup_imgui_core_module();
