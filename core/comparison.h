a2052ba07ea998b6e74ed256deac91056bb86ea6
Patrick Gelvin <patrick@gelvin.dev>
2023-12-04 10:54:39 -0700

    Fix core comparison with CowVec

--- core/comparison.h
+++ core/comparison.h
@@ -2,16 +2,32 @@
 #include <span>
 #include "types.h"
 
+#include "logger.h"
+
 namespace std {
+
+// CowVec iterator comparison is shallow (ptr based), so std::equal is out.
 
 template<typename T>
 bool operator==(const std::span<T>& lhs, const CowVec<T>& rhs) {
-    return std::equal(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+    if (lhs.size() != rhs.size()) { return false; }
+
+    for (size_t index = 0; index < lhs.size(); ++index) {
+        if (lhs[index] != rhs[index]) { return false; }
+    }
+
+    return true;
 }
 
 template<typename T>
 bool operator==(const std::span<const T>& lhs, const CowVec<T>& rhs) {
-    return std::equal(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+    if (lhs.size() != rhs.size()) { return false; }
+
+    for (size_t index = 0; index < lhs.size(); ++index) {
+        if (lhs[index] != rhs[index]) { return false; }
+    }
+
+    return true;
 }
 
 }
