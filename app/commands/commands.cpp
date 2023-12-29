6dd4fbb570434fc5bf9d0bfbcd3b026691e77045
Patrick Gelvin <patrick@gelvin.dev>
2023-12-29 11:47:55 -0700

    Prevent registering command scopes more than once
    Now that we can have multiple Application instances,
    the global command map needs this check

--- app/commands/commands.cpp
+++ app/commands/commands.cpp
@@ -7,6 +7,15 @@ namespace RCalc {
 std::vector<CommandMeta const *> _GlobalCommandMap::commands = {};
 
 void _GlobalCommandMap::register_scope(const ScopeMeta& scope) {
+    static std::vector<const char*> active_scopes;
+
+    // See if scope has been activated before
+    auto it = std::find(active_scopes.begin(), active_scopes.end(), scope.scope_name);
+    if (it != active_scopes.end()) {
+        return;
+    }
+    active_scopes.push_back(scope.scope_name);
+    
     commands.reserve(commands.size() + scope.scope_cmds.size());
 
     for (const CommandMeta* cmd : scope.scope_cmds) {
